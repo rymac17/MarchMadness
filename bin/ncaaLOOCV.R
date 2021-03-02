49d4576c96b0f28f87acfcea6e54661faa506ec7
rymac17 <ryan.mccarley17@gmail.com>
2021-03-02 13:10:29 -0800

    refined loocv validation

--- bin/ncaaLOOCV.R
+++ bin/ncaaLOOCV.R
@@ -5,40 +5,64 @@ source('src/ncaaHelpers.R')
 
 # read tables
 masterTBL <- read.csv('data/masterTBL.csv')
-# statsTBL <- read.csv('data/statsTBL.csv')
-
+statsTBL <- read.csv('data/statsTBL.csv')
 
 # loocv
-test <- unique(masterTBL$year)
-modelTBL <- filter(masterTBL, year!=test[1])
-# model
-library(glmnet)
-x <- as.matrix(select(modelTBL, AdjEM, AdjO, AdjD, AdjT, OppO, OppD,
-                      AdjEM_2, AdjO_2, AdjD_2, AdjT_2, OppO_2, OppD_2,
-                      Losses, Losses_2))
-y <- as.matrix(select(modelTBL, outcome))
-set.seed(1011)
-cv_outcome <- cv.glmnet(x, y, family="binomial", type.measure="auc", nfolds=10, alpha=1)
-coef(cv_outcome, s = "lambda.min")
+loocvBOOT <- lapply(unique(masterTBL$year), function(y){
+  library(dplyr)
+  library(glmnet)
+  yr <<- y
+  
+  # model
+  modelTBL <- filter(masterTBL, year!=yr)
+  x <- as.matrix(select(modelTBL, AdjEM, AdjO, AdjD, AdjT, OppO, OppD,
+                        AdjEM_2, AdjO_2, AdjD_2, AdjT_2, OppO_2, OppD_2,
+                        Losses, Losses_2))
+  y <- as.matrix(select(modelTBL, outcome))
+  set.seed(1011)
+  cv_outcome <<- cv.glmnet(x, y, family="binomial", type.measure="auc", nfolds=10, alpha=1)
+  coef(cv_outcome, s = "lambda.min")
 
-teams <- mkBracket(yr=test[1])
+  teams <- mkBracket(yr)
 
-obsDFT <- masterTBL %>% 
-  filter(year==test[1]) %>% 
-  mutate(winner=ifelse(score>score_2, team, team_2)) %>% 
-  dplyr::select(round, region_name, winner)
+  obsDFT <- masterTBL %>% 
+    filter(year==yr) %>% 
+    mutate(winner=ifelse(score>score_2, team, team_2)) %>% 
+    dplyr::select(round, region_name, winner)
 
-boot <- sapply(1:500, function(x){
-  r <- teams
-  predDFT <- data.frame()
-  for (i in 1:6){
-    r <- runRND(r, i)
-    predDFT <- rbind(predDFT, r %>% dplyr::select(round, region_name=Region, winner=Team))
-  }
-  j <- inner_join(obsDFT, predDFT, by=c('round','region_name','winner')) %>% 
-    mutate(RNDpts=case_when(round==1~1, round==2~2, round==3~4, round==4~8, 
-                            round==5~16, round==6~32)) %>% 
-    summarise(pts=sum(RNDpts), pct=pts/192)
-  return(j %>% pull(pts))
+  boot <- sapply(1:500, function(x){
+    r <- teams
+    predDFT <- data.frame()
+    for (i in 1:6){
+      r <- runRND(r, i)
+      predDFT <- rbind(predDFT, r %>% dplyr::select(round, region_name=Region, winner=Team))
+    }
+    j <- inner_join(obsDFT, predDFT, by=c('round','region_name','winner')) %>% 
+      mutate(RNDpts=case_when(round==1~1, round==2~2, round==3~4, round==4~8, 
+                              round==5~16, round==6~32)) %>% 
+      summarise(pts=sum(RNDpts), pct=pts/192)
+    return(j %>% pull(pts))
+  })
+
+  return(boot)
 })
+
+names(loocvBOOT) <- unique(masterTBL$year)
+saveRDS(loocvBOOT, 'data/loocvBOOT.rds')
+
+
+library(ggplot2)
+newdft <- data.frame(v=do.call('c',loocvBOOT), year=do.call('c', lapply(names(loocvBOOT), function(x) rep(x,500))))
+
+ggplot(data=newdft, aes(x=v, y=..density.., col=year)) +
+  geom_density()
+
+
+ggplot(data=newdft, aes(x=v, y=..density..)) +
+  geom_density()
+
+summary(newdft$v)
+
+ggplot(data=newdft, aes(y=v)) +
+  geom_boxplot()
 
