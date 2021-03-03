e15e35673ac5d0515974c1044b793e024231a606
rymac17 <ryan.mccarley17@gmail.com>
2021-03-03 12:49:59 -0800

    run with different sample sizes

--- bin/ncaaLOOCV.R
+++ bin/ncaaLOOCV.R
@@ -12,6 +12,7 @@ loocvBOOT <- lapply(unique(masterTBL$year), function(y){
   library(dplyr)
   library(glmnet)
   yr <<- y
+  sampleSize <<- 100
   
   # model
   modelTBL <- filter(masterTBL, year!=yr)
@@ -48,15 +49,28 @@ loocvBOOT <- lapply(unique(masterTBL$year), function(y){
 })
 
 names(loocvBOOT) <- unique(masterTBL$year)
-saveRDS(loocvBOOT, 'data/loocvBOOT.rds')
+saveRDS(loocvBOOT, 'data/loocvBOOT_SS100.rds')
 
+
+loocvBOOT <- readRDS('data/loocvBOOT.rds')
+loocvBOOT <- readRDS('data/loocvBOOT_SS10.rds')
+loocvBOOT <- readRDS('data/loocvBOOT_SS100.rds')
 
 library(ggplot2)
 newdft <- data.frame(v=do.call('c',loocvBOOT), year=do.call('c', lapply(names(loocvBOOT), function(x) rep(x,500))))
 
+
+ggplot(data=newdft %>% filter(year %in% 2011:2018), aes(x=v, y=..density.., col=year)) +
+  geom_density()
+summary(newdft %>% filter(year %in% 2011:2018) %>% pull(v))
+summary(newdft %>% filter(year==2015) %>% pull(v))
+
 ggplot(data=newdft, aes(x=v, y=..density.., col=year)) +
   geom_density()
 
+ggplot(data=newdft %>% filter(year==2019), aes(x=v, y=..density..)) +
+  geom_density()
+summary(newdft %>% filter(year==2019) %>% pull(v))
 
 ggplot(data=newdft, aes(x=v, y=..density..)) +
   geom_density()
