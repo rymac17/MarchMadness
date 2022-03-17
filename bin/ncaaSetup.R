4881bab6b4645c032f7140cb8a2f80a3d9bdb2d4
rymac17 <ryan.mccarley17@gmail.com>
2022-03-17 09:37:49 -0700

    work on seeding analysis

--- bin/ncaaSetup.R
+++ bin/ncaaSetup.R
@@ -45,8 +45,75 @@ masterTBL <- left_join(scores, stats, by=c('team'='Team','year'='year')) %>%
          margin=score/(score+score_2))
 write.csv(masterTBL, 'C:/Users/ryanm/Dropbox/R/MarchMadness_data/masterTBL.csv', row.names=F)
 
+
+
+
+### check stats
+library(dplyr)
+library(xlsx)
+statsTBL <- read.csv('C:/Users/ryanm/Dropbox/R/MarchMadness_data/statsTBL.csv')
+masterTBL <- read.csv('C:/Users/ryanm/Dropbox/R/MarchMadness_data/masterTBL.csv')
+teamsTBL <- read.xlsx('C:/Users/ryanm/Dropbox/R/MarchMadness_data/teams/teams2022.xlsx', sheetName='Sheet1')
+
+t <- 'Gonzaga'
+left_join(
+  masterTBL %>% 
+    filter(team==t | team_2==t) %>% 
+    mutate(TEAM = t, YEAR = year,
+           SEED = case_when(team==t ~ seed, team_2==t ~ seed_2),
+           WIN = case_when(team==t ~ outcome, team_2==t ~ +!outcome),
+           .keep='none') %>% 
+    group_by(YEAR) %>% 
+    summarise(TEAM = first(TEAM),
+              SEED = first(SEED),
+              WINS = sum(WIN),
+              FF = ifelse(WINS>=4, T, F),
+              NC = ifelse(WINS==6, T, F)),
+  statsTBL %>% filter(Team==t),
+  by=c('YEAR'='year')
+) %>% 
+  left_join(., avgSeedLUT %>% dplyr::select(SEED, AVG), by='SEED') %>% 
+  mutate(plusMinus=AdjEM-AVG)
+
+
+
 historicalStats <- masterTBL %>% 
+  filter(year!=2022) %>% 
   group_by(seed, seed_2) %>% 
   summarise(pctUpsets=round(sum(upset)/n(),2), 
             events=n(),
             upsets=sum(upset))
+
+historicalStats %>% group_by(seed_2) %>% summarise(cumprob=sum(pctUpsets)) %>% arrange(cumprob)
+
+
+# average AdjEM for seed
+avgSeedLUT <- rbind(masterTBL %>% dplyr::select(SEED=seed, KP=AdjEM), masterTBL %>% dplyr::select(SEED=seed_2, KP=AdjEM_2)) %>% 
+  group_by(SEED) %>% 
+  summarise(MIN=quantile(KP, probs=0), 
+            Q1=quantile(KP, probs=.25), 
+            MED=quantile(KP, probs=.5),
+            AVG=mean(KP),
+            Q3=quantile(KP, probs=.75),
+            MAX=quantile(KP, probs=1))
+
+# which teams are better than an average of that seed?
+seedTest <- left_join(teamsTBL, statsTBL %>% filter(year==2022), by='Team') %>% 
+  left_join(., avgSeedLUT, by=c('Seed'='SEED')) %>% 
+  mutate(plusMinus=AdjEM-AVG)
+  arrange(desc(plusMinus))
+
+df <- rbind(masterTBL %>% dplyr::select(seed, Rk, AdjEM, AdjO, AdjD, AdjT, Luck, AdjEM.1, OppO, OppD, Wins, Losses, OppEM),
+            masterTBL %>% dplyr::select(seed=seed_2, Rk=Rk_2, AdjEM=AdjEM_2, AdjO=AdjO_2, AdjD=AdjD_2, AdjT=AdjT_2, Luck=Luck_2, 
+                                        AdjEM.1=AdjEM.1_2, OppO=OppO_2, OppD=OppD_2, Wins=Wins_2, Losses=Losses_2, OppEM=OppEM_2)) %>% 
+  mutate(seed=factor(seed))
+
+library(randomForest)
+forest <- randomForest(seed ~ ., data=df, importance=T, proximity=T)
+print(forest)
+
+seedTest['rf_seed'] <- predict(forest, seedTest)
+seedTBL <- seedTest %>% 
+  dplyr::select(Team, Seed, rf_seed, plusMinus) %>% 
+  mutate(reseedMargin=as.numeric(Seed)-as.numeric(rf_seed)) %>% 
+  arrange(desc(plusMinus))
